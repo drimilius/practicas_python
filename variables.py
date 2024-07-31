7bacbe64eda36a5e451dc2c209962355e2bcdbdc
ToIvanP <drimilius@gmail.com>
2024-07-31 10:54:27 -0400

    agregando archivo de variables python

---
+++ variables.py
@@ -0,0 +1,22 @@
+a=10
+b=10
+print (a)
+print (b)
+
+#variable flotante
+c=1.5
+d=3.7
+suma =c+ d
+print (suma)
+
+#variable de tipo string
+nombre = 'Roberto'
+apellido = 'Hernandez'
+fullName = nombre + ' ' + apellido  
+print (fullName)
+#variable booleana
+lampara=True
+print (lampara)
+lampara=False
+print (lampara)
+
