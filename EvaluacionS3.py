5b2e4bd40584f5ef1659c566b790e622b9fc75ad
ToIvanP <drimilius@gmail.com>
2024-07-31 15:29:58 -0400

    Create EvaluacionS3.py

---
+++ EvaluacionS3.py
@@ -0,0 +1,6 @@
+#importar la libreria math para usar funciones matematicas
+import math 
+
+y = 81 #definir la variable
+resultado = math.sqrt(y) #utilizar la funcion sqrt para calcular la raiz cuadrada de una variable
+print(resultado) #
