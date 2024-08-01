63a28f301d1b9f6c84d74265eb12204a9364eb8e
ToIvanP <drimilius@gmail.com>
2024-08-01 18:35:39 -0400

    Create EvaluacionS4.py

---
+++ EvaluacionS4.py
@@ -0,0 +1,30 @@
+#creacion de diccionario evaluacion S4
+
+persona1 = {
+    
+    "name": "Ivan",
+    "apellidos": "pino",
+    "telefono": "123456789",
+}
+
+persona2 = {
+    
+    "name": "Pedro",
+    "apellidos": "lopez",
+    "telefono": "987654321",
+}
+
+persona3 = {
+    
+    "name": "Maria",
+    "apellidos": "Gomez",
+    "telefono": "321654987",
+   
+}
+
+#almacenar los dic en una lista
+lista_personas = [persona1, persona2, persona3] 
+
+#imprimir la lista en pantalla
+print(lista_personas)
+
