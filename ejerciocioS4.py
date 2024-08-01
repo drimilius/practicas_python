168a5dd9be0364af31e221ddea960e2bf8935ef2
ToIvanP <drimilius@gmail.com>
2024-08-01 18:25:58 -0400

    Create ejerciocioS4.py

---
+++ ejerciocioS4.py
@@ -0,0 +1,14 @@
+#crear las variantes
+numero_entero = 8
+numero_flotante = 10.5
+palabra = "ejercicio"
+
+#crear un ser que contenga las variables creadas
+set = {numero_entero, numero_flotante, palabra}
+
+#crear una lista que contengga el set y una variable con el valor logico false
+
+lista = [set, False]
+
+#imprimir la lista en pantalla
+print(lista)
