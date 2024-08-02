a134f18462cdbfbd2cfcec1b3cae39c586f3f91a
ToIvanP <drimilius@gmail.com>
2024-08-02 11:19:44 -0400

    operadores
    f-string
    guide reference
    operador in

---
+++ f-string.py
@@ -0,0 +1,15 @@
+#ejemplos de uso de f-strings
+
+nombre = "Roberto"
+apellido = "Hernandez"
+edad = 38
+print(f"Hola, mi nombre es {nombre} {apellido} y tengo {edad} años.")
+#ejemplo de suma f-strings
+precio1=12345
+precio2=312
+print(f"el total de tu compra es: ${precio1 + precio2}")
+# if dentro de un print
+temperatura = 30
+print(f"La temperatura es esta {"alta" if temperatura >25 else "baja"}")
+
+
---
+++ operador_in.py
@@ -0,0 +1,11 @@
+frutas=["manzana", "pera", "uva", "kiwi", "mango"]
+if "manzana" in frutas:
+    print ("la manzana si esta")
+#recorriendo una cadena de texto
+mensaje = "Hola desde mexico con tormenta tropical"
+if "mexico" in mensaje:
+    print("si esta la palabra mexico")
+    
+    #bucles
+    
+    
