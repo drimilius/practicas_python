83ce929611f34cb37d77c0a51b9f52acca5f31d0
ToIvanP <drimilius@gmail.com>
2024-08-13 16:01:44 -0400

    Create ejercicioS8.py

---
+++ ejercicioS8.py
@@ -0,0 +1,13 @@
+#lista
+mi_lista = [5, 20, 15, 20, 25, 50, 20, 5, 18, 15]
+
+#eliminar duplicados
+duplicados = list(set(mi_lista))
+
+#ordenar la lista
+
+duplicados.sort()
+
+print("lista sin duplicados y ordenada:")
+print(duplicados)
+
