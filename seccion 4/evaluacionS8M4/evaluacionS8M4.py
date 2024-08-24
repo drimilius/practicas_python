51970c3f0cf6f4e784324310969107a828ca58ab
ToIvanP <drimilius@gmail.com>
2024-08-24 17:49:34 -0400

    evaluacion S8M4

--- seccion 4/evaluacionS8M4/evaluacionS8M4.py
+++ seccion 4/evaluacionS8M4/evaluacionS8M4.py
@@ -1,16 +1,16 @@
 try:
-    with open("seccion 4/ejercicioS8M4/informacion.dat","x") as file:
+    with open("seccion 4/evaluacionS8M4/informacion.dat","x") as file:
         file.write("Datos de información en la línea 1.\n")
         file.write("Datos de información en la línea 2.\n")
         file.write("Datos de información en la línea 3.\n")
         file.write("Datos de información en la línea 4.\n")
         file.write("Datos de información en la línea 5.\n")
 except FileExistsError:
-    print("El archivo ya existe")
+    print("El archivo ya existe, ha sido creado previamente")
     
 
 #manejo de errores
-with open("seccion 4/ejercicioS8M4/informacion.dat","r") as file:
+with open("seccion 4/evaluacionS8M4/informacion.dat","r") as file:
     try:
         contenido = file.read()
         print(contenido)
