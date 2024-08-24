65134f5ac0efe9f81ea012eae771e6aca4e7452a
ToIvanP <drimilius@gmail.com>
2024-08-24 17:43:33 -0400

    ejercicio S8 M4

---
+++ seccion 4/ejercicioS8M4/ejercicioS8M4.py
@@ -0,0 +1,20 @@
+try:
+    with open("seccion 4/ejercicioS8M4/informacion.dat","x") as file:
+        file.write("Datos de información en la línea 1.\n")
+        file.write("Datos de información en la línea 2.\n")
+        file.write("Datos de información en la línea 3.\n")
+        file.write("Datos de información en la línea 4.\n")
+        file.write("Datos de información en la línea 5.\n")
+except FileExistsError:
+    print("El archivo ya existe.")
+    
+
+#manejo de errores
+with open("seccion 4/ejercicioS8M4/informacion.dat","r") as file:
+    try:
+        contenido = file.read()
+        print(contenido)
+    except FileNotFoundError:
+        print("No se encuentra este archivo.") 
+    except IOError as e:
+        print("el archivo no se encuentra")
